06b7c1de29282b7f6abe1642626050da7147dbc3
Ilyes-Cheikh <Cheikh.ilyes10@gmail.com>
2021-10-25 22:40:01 +0100

    responsive adjust

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -20,8 +20,9 @@ export default function Home() {
             <PresForum />
             <Funfact />
             <br/>
-            <Entreprises/>
             <Aftermovie/>
+            <Entreprises/>
+            <br/>
             <Footer />
         </div>
 
