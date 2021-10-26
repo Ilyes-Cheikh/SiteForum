b81001a4bf61092c8ba0330069c0ff75e5a3f11c
Ilyes-Cheikh <Cheikh.ilyes10@gmail.com>
2021-10-26 23:12:42 +0100

    last adjust

--- Client/src/Components/Entreprises.jsx
+++ Client/src/Components/Entreprises.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import '../Assets/css/entreprises.css'
 import Zoom from 'react-reveal/Zoom';
 import img1 from '../Assets/images/AxeFinance.png'
-import img2 from '../Assets/images/partenaires/cognira.jpg'
+import img4 from '../Assets/images/partenaires/cognira.jpg'
 import img3 from '../Assets/images/habumus.png'
-import img4 from '../Assets/images/logo-infor.png'
-import img5 from '../Assets/images/machinestalk.png'
-import img6 from '../Assets/images/Minotore.png'
+import img2 from '../Assets/images/logo-infor.png'
+import img5 from '../Assets/images/Minotore.png'
+import img6 from '../Assets/images/machinestalk.png'
 import img7 from '../Assets/images/talan.png'
 import img8 from '../Assets/images/value.png'
 import img9 from '../Assets/images/Linedata_Logo.png'
@@ -26,7 +26,7 @@ export default function Entreprises() {
                     <span style={{ "--i": 5 }}><img src={img5} /></span>
                     <span style={{ "--i": 6 }}><img src={img6} /></span>
                     <span style={{ "--i": 7 }}><img src={img7} /></span>
-                    <span style={{ "--i": 8 }}><img src={img8} /></span>
+                    <span style={{ "--i": 8  }}><img src={img8} /></span>
                     <span style={{ "--i": 9 }}><img src={img9} /></span>
                     <span style={{ "--i": 10}}><img src={img10} /></span>
                 </div>
